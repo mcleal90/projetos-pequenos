10655e860b860db5ecbd4d4a616fb38895c9fa19
Matheus Leal <s.matheus.l@gmail.com>
2022-08-22 11:09:45 -0300

    new file:   Calculator/index.html 	new file:   Calculator/script.js 	new file:   Calculator/style.css 	modified:   Media/style.css 	new file:   ToDoList/index.html 	new file:   ToDoList/script.js 	new file:   ToDoList/style.css

---
+++ Calculator/script.js
@@ -0,0 +1,8 @@
+function insert(num) {
+    // var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = num;
+}
+
+function clear() {
+    document.getElementById('resultado').innerHTML = "";
+}
---
+++ ToDoList/script.js
@@ -0,0 +1,3 @@
+function add () {
+    txtToDo = document.getElementById(txtInput);
+}
