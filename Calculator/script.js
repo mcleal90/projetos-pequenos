15d01a55ea19d444feb7d20f341b62220d66ca49
Matheus Leal <s.matheus.l@gmail.com>
2022-08-22 11:55:47 -0300

    commit 2208

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,8 +1,32 @@
+// TODO
+// Hide operations and create two different vars when operation is clicked(OldNum, NewNum) 
+// Disallow . from being entered multiple times
+
 function insert(num) {
-    // var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = num;
+    var numero = document.getElementById('resultado').innerHTML;
+    if (numero === '0' ) {
+        document.getElementById('resultado').innerHTML = num;
+    } else {
+        document.getElementById('resultado').innerHTML = numero + num;
+    }
 }
 
-function clear() {
-    document.getElementById('resultado').innerHTML = "";
+function clean() {
+    document.getElementById('resultado').innerHTML = "0";
 }
+
+
+function equals() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
+}
+
+// UNUSED
+// function back() {
+//     var resultado = document.getElementById('resultado').innerHTML;
+//     if (resultado != "0") {
+//         document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+//     }
+// }
