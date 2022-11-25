79e6981c4a760169ae5a2f1c1350995e1404f162
Matheus Leal <s.matheus.l@gmail.com>
2022-11-25 13:56:03 -0300

    producao 25112022

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,3 +1,10 @@
-function add () {
-    txtToDo = document.getElementById(txtInput);
-}
+let inputTask = document.querySelector('#inputTask');
+let btAdd = document.querySelector('#btAdd');
+let taskList = document.querySelector('#taskList');
+
+// Adicionar ao apertar Enter
+inputTask.addEventListener('keypress', (e) => {
+    if (e.keyCode == 13) {
+        
+    }
+})
